2b6205563e8bd4bd4a3360eac21450972e78262f
lmalmud <lmalmud2@jh.edu>
2025-05-27 23:32:17 -0400

    renamed the columns, building compute_savings_hazira

--- ai_scenario_simulation/compute_savings_hazira.py
+++ ai_scenario_simulation/compute_savings_hazira.py
@@ -7,10 +7,30 @@ summarize savings by subprocess and total.
 '''
 
 import pandas as pd
+from pathlib import Path
 
-NUM_QUAY = 6
-NUM_YARD = 14
-unit_costs = pd.read_excel('../baseline_cost_model_inputs/unit_costs_hazira.xlsx', 'unit_costs')
-base = pd.read_excel('../simulation_tasks/hazira_monthly_metrics.xlsx', 'Sheet1')
+CONFIG = {
+    "unit_rates" : Path("../baseline_cost_model_inputs/unit_costs_hazira.xlsx"),
+    "baseline_xlsx" : Path("../simulation_tasks/hazira_monthly_metrics.xlsx"),
+    "output_xlsx" : Path("Cost_Savings_Summary.xlsx"),
+    "scenario_glob" : Path("Adjusted_Metrics_SC_*.xlsx") # Will match any pattern of adjusted metrics
+}
 
-# TODO: calculate the baseline costs and the new costs for each simulation
+def load_unit_rates(path: Path) -> pd.Series:
+    '''
+    Returns a dataframe, indexed by resource, with appropriate unit rates.
+    Parameters
+    path (Path): path to Excel spreadsheet containing unit rates
+    '''
+    df = pd.read_excel(path, sheet_name="unit_costs")
+    df = df.set_index("metric")["unit_rate"] # metric is each resource: quay_crane, yard_crane, etc.
+    return df
+
+def annualize_metrics(df: pd.DataFrame) -> pd.Series:
+    '''
+    Sum columns that are numeric; assume one column per metric.
+    '''
+    numeric = df.select_dtypes("number")
+    return numeric.sum()
+
+print(load_unit_rates(CONFIG["unit_rates"]))
